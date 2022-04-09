38f302ac0ca6073b739dbb18c221eee2d146313b
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-09 16:00:43 +0530

    [Vidya] Refactored : Checked The Equality Of Two Lines

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -5,8 +5,15 @@ namespace LineComparison
     {
         static void Main(String[] args)
         {
-            LengthOfLine computation = new LengthOfLine(35, 74, 82, 100);
-            computation.CalculateLength();
+            LengthOfLine computation = new LengthOfLine(30, 65, 87, 100);
+            double line1 = computation.CalculateLength();
+            LengthOfLine line = new LengthOfLine(35, 74, 82, 110);
+            double line2 = line.CalculateLength();
+            if (line1.Equals(line2))
+                Console.WriteLine("Length of both lines are equal");
+            else
+                Console.WriteLine("Length of both lines are not equal");
+                
         }
     }
 }
